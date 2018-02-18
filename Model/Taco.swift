0c3abaa250bcf09ed01c4973c3487a49f6cac5b5
Mina Guirguis <MGuirguis.10@gmail.com>
2018-02-18 01:15:47 -0500

    We created several new classes, protocols and enums for tacos

---
+++ Model/Taco.swift
@@ -0,0 +1,92 @@
+//
+//  Taco.swift
+//  TacoPOP
+//
+//  Created by Mina Guirguis on 2/17/18.
+//  Copyright © 2018 Mina Guirguis. All rights reserved.
+//
+
+import Foundation
+
+enum TacoShell: Int {
+    case Flour = 1
+    case Corn = 2
+}
+
+enum TacoProtein: String {
+    case Beef = "Beef"
+    case Chicken = "Chicken"
+    case Brisket = "Brisket"
+    case Fish = "Fish"
+}
+
+enum TacoCondiment: Int {
+    case Loaded = 1
+    case Plain = 2
+}
+
+struct Taco {
+    private var _id: Int!
+    private var _productName: String!
+    private var _shellId: TacoShell!
+    private var _proteinId: TacoProtein!
+    private var _condimentId: TacoCondiment!
+    
+    //wrapping it in "public acessors" because the variables created above are private ^^
+    var id: Int {
+        return _id
+    }
+    
+    var productName: String {
+        return _productName
+    }
+    
+    var shellId: TacoShell {
+        return _shellId
+    }
+    
+    var proteinId: TacoProtein {
+        return _proteinId
+    }
+    
+    var condimentId: TacoCondiment {
+        return _condimentId
+    }
+    //we are intilizing everything below
+    init(id: Int, productName: String, shellId: Int, proteinId: Int, condimentId: Int) {
+        _id = id
+        _productName = productName
+        
+        //Taco shell switch statment (because its an enum type)
+        switch shellId {
+        case 2:
+            self._shellId = TacoShell.Corn
+        default:
+            self._shellId = TacoShell.Flour
+        }
+        
+        //Taco Protein
+        switch proteinId {
+        case 2:
+            self._proteinId = TacoProtein.Chicken
+        case 3:
+            self._proteinId = TacoProtein.Brisket
+        case 4:
+            self._proteinId = TacoProtein.Fish
+        default:
+            self._proteinId = TacoProtein.Beef
+        }
+        
+        //Taco Condiment
+        switch condimentId {
+        case 2:
+            self._condimentId = TacoCondiment.Plain
+        default:
+            self._condimentId = TacoCondiment.Loaded
+        }
+        
+    }
+}
+
+
+
